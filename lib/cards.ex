bf36c484135d8c1a554ce30412ca1581953e9473
Rus Hughes <russell.hughes@gmail.com>
2018-11-04 20:46:25 +0900

    added some comments

--- lib/cards.ex
+++ lib/cards.ex
@@ -10,12 +10,19 @@ defmodule Cards do
 
   end
 
+  #shuffle the deck
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  #does the deck contain this card?
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  #deal the deck
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 
 end
